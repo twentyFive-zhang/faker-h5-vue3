141d05f1e4a23a5795b25ecfcdb7d6f7bcbb3e3d
twentyFive-zhang <twentyFive-zhang@users.noreply.github.com>
2023-04-26 08:46:52 +0000

    deploy: 66eab54768e23e035b892cc37c901dd96d96c6aa

--- assets/use-id.1be47576.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index.c1a17bf6.js";let o=0;function t(){const t=n(),{name:e="unknown"}=(null==t?void 0:t.type)||{};return`${e}-${++o}`}export{t as u};
---
+++ assets/use-id.aa18ffa5.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index.7edbb798.js";let e=0;function o(){const o=n(),{name:t="unknown"}=(null==o?void 0:o.type)||{};return`${t}-${++e}`}export{o as u};
