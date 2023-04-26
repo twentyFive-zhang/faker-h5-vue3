66eab54768e23e035b892cc37c901dd96d96c6aa
二五 <1097983662@qq.com>
2023-04-26 16:46:12 +0800

    fix: base修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,125 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VantResolve, createStyleImportPlugin } from 'vite-plugin-style-import';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VantResolve, createStyleImportPlugin } from "vite-plugin-style-import";
 // @ts-ignore
-import pxtovw from 'postcss-px-to-viewport'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { resolve } from 'path';
+import pxtovw from "postcss-px-to-viewport";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+
+// console.log(import.meta.env,)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    vueJsx(),
-    createStyleImportPlugin({
-      resolves: [VantResolve()],
-      libs: [
-        {
-          libraryName: 'vant',
-          esModule: true,
-          resolveStyle: component => `/node_modules/vant/es/${component}/style/index`,
-        },
-      ],
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer'),
-        pxtovw({
-          unitToConvert: 'px',
-          viewportWidth: 375,
-          unitPrecision: 5,
-          propList: ['*'],
-          viewportUnit: 'vw',
-          fontViewportUnit: 'vw',
-          selectorBlackList: [],
-          minPixelValue: 1,
-          mediaQuery: false,
-          replace: true,
-          exclude: [],
-          landscape: false,
-          landscapeUnit: 'vw',
-          landscapeWidth: 568
-        })
-      ]
-    }
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
+
+export default ({ mode }) => {
+  console.log(mode)
+  return {
+    base: mode === 'production' ? "/faker-h5-vue3/" : "./",
+    plugins: [
+      vue(),
+      vueJsx(),
+      createStyleImportPlugin({
+        resolves: [VantResolve()],
+        libs: [
+          {
+            libraryName: "vant",
+            esModule: true,
+            resolveStyle: (component) => `/node_modules/vant/es/${component}/style/index`,
+          },
+        ],
+      }),
+    ],
+    css: {
+      postcss: {
+        plugins: [
+          require("autoprefixer"),
+          pxtovw({
+            unitToConvert: "px",
+            viewportWidth: 375,
+            unitPrecision: 5,
+            propList: ["*"],
+            viewportUnit: "vw",
+            fontViewportUnit: "vw",
+            selectorBlackList: [],
+            minPixelValue: 1,
+            mediaQuery: false,
+            replace: true,
+            exclude: [],
+            landscape: false,
+            landscapeUnit: "vw",
+            landscapeWidth: 568,
+          }),
+        ],
+      },
     },
-  },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        //生产环境时移除console
-        drop_console: true,
-        drop_debugger: true,
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    },
+    build: {
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          //生产环境时移除console
+          drop_console: true,
+          drop_debugger: true,
+        },
       },
     },
   }
-})
+};
+// export default defineConfig({
+//   base: './',
+//   plugins: [
+//     vue(),
+//     vueJsx(),
+//     createStyleImportPlugin({
+//       resolves: [VantResolve()],
+//       libs: [
+//         {
+//           libraryName: 'vant',
+//           esModule: true,
+//           resolveStyle: component => `/node_modules/vant/es/${component}/style/index`,
+//         },
+//       ],
+//     }),
+//   ],
+//   css: {
+//     postcss: {
+//       plugins: [
+//         require('autoprefixer'),
+//         pxtovw({
+//           unitToConvert: 'px',
+//           viewportWidth: 375,
+//           unitPrecision: 5,
+//           propList: ['*'],
+//           viewportUnit: 'vw',
+//           fontViewportUnit: 'vw',
+//           selectorBlackList: [],
+//           minPixelValue: 1,
+//           mediaQuery: false,
+//           replace: true,
+//           exclude: [],
+//           landscape: false,
+//           landscapeUnit: 'vw',
+//           landscapeWidth: 568
+//         })
+//       ]
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       "@": resolve(__dirname, "src"),
+//     },
+//   },
+//   build: {
+//     minify: 'terser',
+//     terserOptions: {
+//       compress: {
+//         //生产环境时移除console
+//         drop_console: true,
+//         drop_debugger: true,
+//       },
+//     },
+//   }
+// })
